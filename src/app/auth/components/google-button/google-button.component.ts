f0da36b0f2b118e7383c09d977b5147d4e6bca55
g4brielagarcia26 <156961627+g4brielagarcia26@users.noreply.github.com>
2024-10-01 13:46:03 +0200

    Creación de directorios, componentes, servicios y guardias.

---
+++ src/app/auth/components/google-button/google-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-google-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './google-button.component.html',
+  styleUrl: './google-button.component.css'
+})
+export class GoogleButtonComponent {
+
+}
