15ac17b7a2923423eb1736c96a5532fd8709c9e7
g4brielagarcia26 <156961627+g4brielagarcia26@users.noreply.github.com>
2024-10-30 18:35:42 +0100

    Creo el HTML y doy estilos al formulario de CreateComponent.

--- src/app/user/admin/create/create.component.ts
+++ src/app/user/admin/create/create.component.ts
@@ -1,12 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, FormArray, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export default class CreateComponent {
+  
+  // Inicializar el FormGroup con todos los controles
+  createToolForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    detail: new FormControl(''),
+    description: new FormControl(''),
+    properties: new FormArray([]), // FormArray para propiedades
+    price: new FormControl('', [Validators.required, Validators.min(0)]),
+    subcategory: new FormControl('', Validators.required),
+    video: new FormControl('', Validators.pattern('https?://.*')),
+    images: new FormControl(null), // Control para almacenar las imágenes
+    logo: new FormControl(null) // Control para almacenar el logo
+  });
 
+  propertyInput = new FormControl('');
+
+  // Getter para acceder al FormArray `properties`
+  get properties() {
+    return this.createToolForm.get('properties') as FormArray;
+  }
+
+  // Agregar una característica al presionar Enter
+  addProperty(event: Event): void {
+    event.preventDefault(); // Evita el comportamiento por defecto de Enter en un formulario
+  
+    const value = this.propertyInput.value?.trim() || ''; // Usa '' si es null o undefined
+  
+    if (value && this.properties.length < 6) {
+      this.properties.push(new FormControl(value));
+      this.propertyInput.reset(); // Limpia el campo de entrada
+    }
+  }
+
+  // Eliminar una característica por índice
+  removeProperty(index: number): void {
+    this.properties.removeAt(index);
+  }
 }
